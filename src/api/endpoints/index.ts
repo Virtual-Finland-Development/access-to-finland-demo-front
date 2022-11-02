deb2f0b35840315e9a253cdccb29e2cc91eca854
Lauri Saarela <lauri.saarela@gofore.com>
2022-11-02 14:12:17 +0200

    removed some logs

--- src/api/endpoints/index.ts
+++ src/api/endpoints/index.ts
@@ -1,6 +1,3 @@
-console.log(process.env.REACT_APP_APPLICATION_BASE_URL);
-console.log(process.env.REACT_APP_AUTH_GW_BASE_URL);
-console.log(process.env.REACT_APP_USER_API_BASE_URL);
 export const APP_BASE_URL =
   process.env.REACT_APP_APPLICATION_BASE_URL || 'http://localhost:3000';
 
