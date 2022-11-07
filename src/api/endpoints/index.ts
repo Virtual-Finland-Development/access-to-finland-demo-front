64df6706e9a5e7a30c280d2b166a0036b43ad23e
Lauri Saarela <lauri.saarela@gofore.com>
2022-11-07 13:55:26 +0200

    testing environment secrets in workflow, again

--- src/api/endpoints/index.ts
+++ src/api/endpoints/index.ts
@@ -10,6 +10,10 @@ export const APP_BASE_URL = (() => {
   }
 })();
 
+console.log(process.env.REACT_APP_AUTH_GW_BASE_URL);
+console.log(process.env.REACT_APP_USER_API_BASE_URL);
+console.log(process.env.REACT_APP_EXT_REGISTRATION_SERVICE_URL);
+
 export const AUTH_GW_BASE_URL =
   process.env.REACT_APP_AUTH_GW_BASE_URL ||
   'https://q88uo5prmh.execute-api.eu-north-1.amazonaws.com';
