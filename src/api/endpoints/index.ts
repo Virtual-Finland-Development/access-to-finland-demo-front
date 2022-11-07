525524325a09e0632efab39327e5f47d26f1c3a8
Lauri Saarela <lauri.saarela@gofore.com>
2022-11-07 14:07:43 +0200

    environments secrets testing testing

--- src/api/endpoints/index.ts
+++ src/api/endpoints/index.ts
@@ -10,9 +10,12 @@ export const APP_BASE_URL = (() => {
   }
 })();
 
-console.log(process.env.REACT_APP_AUTH_GW_BASE_URL);
-console.log(process.env.REACT_APP_USER_API_BASE_URL);
-console.log(process.env.REACT_APP_EXT_REGISTRATION_SERVICE_URL);
+console.log('AUTH_GW_BASE_URL', process.env.REACT_APP_AUTH_GW_BASE_URL);
+console.log('USER_API_BASE_URL', process.env.REACT_APP_USER_API_BASE_URL);
+console.log(
+  'EXT_REGISTRATION_SERVICE_URL',
+  process.env.REACT_APP_EXT_REGISTRATION_SERVICE_URL
+);
 
 export const AUTH_GW_BASE_URL =
   process.env.REACT_APP_AUTH_GW_BASE_URL ||
