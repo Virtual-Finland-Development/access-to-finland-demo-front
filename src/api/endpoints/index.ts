ddeacffd1ce6593ff986e804e563100fe49ea61b
Lauri Saarela <lauri.saarela@gofore.com>
2022-11-08 08:14:30 +0200

    removed some env variable logs

--- src/api/endpoints/index.ts
+++ src/api/endpoints/index.ts
@@ -10,13 +10,6 @@ export const APP_BASE_URL = (() => {
   }
 })();
 
-console.log('AUTH_GW_BASE_URL', process.env.REACT_APP_AUTH_GW_BASE_URL);
-console.log('USER_API_BASE_URL', process.env.REACT_APP_USER_API_BASE_URL);
-console.log(
-  'EXT_REGISTRATION_SERVICE_URL',
-  process.env.REACT_APP_EXT_REGISTRATION_SERVICE_URL
-);
-
 export const AUTH_GW_BASE_URL =
   process.env.REACT_APP_AUTH_GW_BASE_URL ||
   'https://q88uo5prmh.execute-api.eu-north-1.amazonaws.com';
