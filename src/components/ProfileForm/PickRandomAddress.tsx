c93fbeebe6a15c006e93fe2287167fc21048dfb6
lassi.patanen <lassi.patanen@gofore.com>
2022-11-18 15:51:50 +0200

    Updated address generator to return Address object

--- src/components/ProfileForm/PickRandomAddress.tsx
+++ src/components/ProfileForm/PickRandomAddress.tsx
@@ -1,9 +1,23 @@
 import addresses from './fakeData/addresses.json';
+import { Address } from '../../@types';
 
 export const pickRandomAddress = () => {
   const number = Math.floor(Math.random() * addresses.length);
-  const address = addresses[number];
-  return address
-    ? `${number} ${address.detail} ${address.name}`
-    : 'Random address 123';
+  const randomAddress = addresses[number];
+  if (randomAddress) {
+    let formattedAddress: Address = {
+      street_address: randomAddress.detail + ' street',
+      city: randomAddress.detail.split(',')[0],
+      country: 'United States',
+      zip_code: randomAddress.name,
+    };
+    return formattedAddress;
+  }
+
+  return {
+    street_address: 'Mannerheimintie 42',
+    city: 'Helsinki',
+    zip_code: '00322',
+    country: 'Suomi',
+  };
 };
