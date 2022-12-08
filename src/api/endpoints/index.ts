76b5ca10ceb5a77014f2e5adc5edc5056df0ca17
Lauri Saarela <lauri.saarela@gofore.com>
2022-12-08 14:11:13 +0200

    added utility function to remove trailing slash from string, use with users-api/testbed-api endpoints

--- src/api/endpoints/index.ts
+++ src/api/endpoints/index.ts
@@ -1,3 +1,6 @@
+// utils
+import { removeTrailingSlash } from '../../utils';
+
 export const APP_BASE_URL = (() => {
   const {
     location: { protocol, hostname, port },
@@ -13,17 +16,14 @@ export const APP_BASE_URL = (() => {
 export const AUTH_GW_BASE_URL =
   process.env.REACT_APP_AUTH_GW_BASE_URL ||
   'https://q88uo5prmh.execute-api.eu-north-1.amazonaws.com';
-console.log(AUTH_GW_BASE_URL || 'no auth-gw url');
 
-export const USERS_API_BASE_URL =
-  process.env.REACT_APP_USERS_API_BASE_URL || 'http://localhost:5001';
-console.log(USERS_API_BASE_URL || 'no users-api url');
+export const USERS_API_BASE_URL = process.env.REACT_APP_USERS_API_BASE_URL
+  ? removeTrailingSlash(process.env.REACT_APP_USERS_API_BASE_URL)
+  : 'http://localhost:5001';
 
-export const TESTBED_API_BASE_URL =
-  process.env.REACT_APP_TESTBED_API_BASE_URL ||
-  'https://v2xw5b7nhqkduxcw6v56pas5yy0livjb.lambda-url.eu-north-1.on.aws';
-console.log(TESTBED_API_BASE_URL || 'no testbed-api url');
+export const TESTBED_API_BASE_URL = process.env.REACT_APP_TESTBED_API_BASE_URL
+  ? removeTrailingSlash(process.env.REACT_APP_TESTBED_API_BASE_URL)
+  : 'https://v2xw5b7nhqkduxcw6v56pas5yy0livjb.lambda-url.eu-north-1.on.aws';
 
 export const EXT_REGISTRATION_SERVICE_URL =
   process.env.REACT_APP_EXT_REGISTRATION_SERVICE_URL || 'http://localhost:3001';
-console.log(EXT_REGISTRATION_SERVICE_URL || 'no external-service-demo url');
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -26,3 +26,8 @@ export function isNewUser(user: Partial<UserProfile>) {
   const { created, modified } = user;
   return isSameSecond(parseISO(created!), parseISO(modified!));
 }
+
+// Helper function to remove trailing slashes from string
+export function removeTrailingSlash(str: string) {
+  return str.endsWith('/') ? str.slice(0, -1) : str;
+}
