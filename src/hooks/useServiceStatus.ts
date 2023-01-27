b1e360c261e661a5b9615a662a408f0edd893d39
Lauri Saarela <lauri.saarela@gofore.com>
2023-01-27 09:36:45 +0200

    useServiceStatus -hook: silence the not found error

--- src/hooks/useServiceStatus.ts
+++ src/hooks/useServiceStatus.ts
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 
 // hooks
@@ -7,18 +8,29 @@ import useErrorToast from './useErrorToast';
 import api from '../api';
 
 export default function useServiceStatus() {
+  const [error, setError] = useState<any>(null);
+
   const query = useQuery(
     ['service-status'],
     async () => await api.data.getServiceStatus(),
     {
       refetchOnWindowFocus: false,
+      retry: false,
+      onError: (error: any) => {
+        if (
+          error.response?.data?.data?.message &&
+          error.response.data.data.message !== 'Status information not found'
+        ) {
+          setError(error);
+        }
+      },
     }
   );
 
   // display error in toast, if any
   useErrorToast({
     title: 'Could not fetch service status',
-    error: query.error,
+    error: error,
   });
 
   return query;
