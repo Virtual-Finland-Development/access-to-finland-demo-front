22f680d7db2cf101948f80a5fbe47ef7072ae1cf
Lassi Piironen <lsipii@kapsi.fi>
2023-02-27 10:06:23 +0200

    feat: update success param handling to reflect changes in authgw

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -36,7 +36,7 @@ export default function Auth() {
   const queryParams = useMemo(() => new URLSearchParams(search), [search]);
   const eventParam = queryParams.get('event');
   const authProviderParam = queryParams.get('provider');
-  const successParam = queryParams.get('success');
+  const successParam = queryParams.get('success') === 'true';
   const messageParam = queryParams.get('message');
 
   const loginCodeParam = eventParam === "login" && successParam && queryParams.get('loginCode');
