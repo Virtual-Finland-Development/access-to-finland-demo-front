2cc7d92f20f4d5053162db0d629b7d971bbb6028
Lassi Piironen <lsipii@kapsi.fi>
2023-11-24 15:44:04 +0200

    refactor: better typings and comments for the consent feat

--- src/context/ConsentContext/ConsentContextFactory.tsx
+++ src/context/ConsentContext/ConsentContextFactory.tsx
@@ -28,7 +28,7 @@ function getDefaultContextState(
 ): ConsentContextState<ConsentDataSource> {
   return {
     dataSource: dataSource,
-    consentSituation: { consentStatus: '' },
+    consentSituation: { consentStatus: 'noConsent' },
     isConsentInitialized: false,
     isConsentGranted: false,
     redirectToConsentService: () => {},
@@ -53,7 +53,7 @@ function getConsentProvider(
     // Reactive flags and states
     //
     const [consentSituation, setConsentSituation] = useState<ConsentSituation>({
-      consentStatus: '',
+      consentStatus: 'noConsent',
     });
     const [isConsentInitialized, setIsConsentInitialized] =
       useState<boolean>(false);
@@ -99,7 +99,7 @@ function getConsentProvider(
         storedConsentToken
       );
 
-      if (responseSituation.consentToken) {
+      if (responseSituation.consentStatus === 'consentGranted') {
         sessionStorage.setItem(consentStoreKey, responseSituation.consentToken);
       } else if (storedConsentToken) {
         sessionStorage.removeItem(consentStoreKey);
